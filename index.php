ec623688ddf218844a0c0c0a1b89633eadf03576
AgungBarlianSaputra1 <aliviaabadi007@gmail.com>
2026-05-07 15:33:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?='hi';?>
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$koneksi = mysqli_connect(
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port
+);
+
+if(!$koneksi){
+    die("Koneksi gagal : " . mysqli_connect_error());
+}
+?>
